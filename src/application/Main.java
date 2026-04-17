19d00257b610019b974bf3e7c59139f8fbc353f9
gbsalermo <enzogabrielskull@gmail.com>
2026-04-16 23:51:24 -0300

    Adicionei a interface vazia, a classe da tela principal do ui e tambem o run.bat para compilação(Estarei adicionando como funciona no readme)

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,13 @@
 package application;
+
+import javax.swing.JFrame;
+
+import ui.TelaPrincipal;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        new TelaPrincipal();
     }
 }
+
+
