8e1d4d943d3995a282021b4646d26e04ba1a36eb
dead-silver <eduardomenezes.exe@gmail.com>
2026-04-22 01:18:14 -0300

    adicionada classe remover, e interface para linux em run.sh

--- run.sh
+++ run.sh
@@ -7,7 +7,8 @@ echo "Criando pasta bin..."
 mkdir bin
 
 echo "Compilando projeto..."
-javac -d bin src/application/Main.java src/ui/TelaPrincipal.java src/model/.java src/datastructure/.java
+javac -d bin \
+    $(find src -name "*.java" -type f)
 
 if [ $? -ne 0 ]; then
     echo "Erro na compilacao!"
--- src/services/Remover.java
+++ src/services/Remover.java
@@ -3,20 +3,52 @@ package services;
 //imports necessarios
 import datastructure.ListaBiblioteca;
 import model.No;
+import ui.TelaPrincipal;
 
 public class Remover {
-    //metodo para exclusão de elemento
-    public String excluir(ListaBiblioteca lista, int valor){
-        //verificando se a lista está vazia
-        if(lista.getCabeça()== null){
-            return "A lista esta vazia!!";
+    //declaracao de objetos necessarios
+    public String excluirPos(ListaBiblioteca lista, int posicao) {
+    String msg = "";
+    //chamando metodo para percorrer a lista
+    No noTemp = getNo(lista, posicao);
+    
+    //verificando se valor inserido eh valido, caso sim, o no eh removido
+    if (noTemp == null) {
+        msg = "Posicao não existe";
+    } else if (posicao == 0) {
+        if (noTemp.proximo == null) {
+            lista.cabeca = null;
+            lista.cauda = null;
+            msg = "posicao excluida: " + posicao + ", valor: " + noTemp.livro.getTitulo();
+        } else {
+            lista.cabeca = noTemp.proximo;
+            lista.cabeca.anterior = null;
+            msg = "posicao excluida: " + posicao + ", valor: " + noTemp.livro.getTitulo();
         }
-        //getter para percorrer a lista
-        No atual = lista.getCabeça();
-        //percorrer lista
-        while(atual!= null){
-
-        }
-
+    } else if (posicao == lista.tamanho - 1) {
+        lista.cauda = noTemp.anterior;
+        lista.cauda.proximo = null;
+        msg = "posicao excluida: " + posicao + ", valor: " + noTemp.livro.getTitulo();
+    } else {
+        noTemp.anterior.proximo = noTemp.proximo;
+        noTemp.proximo.anterior = noTemp.anterior;
+        msg = "posicao excluida: " + posicao + ", valor: " + noTemp.livro.getTitulo();
     }
+    //reduzindo tamanho da lista
+    lista.tamanho--;
+    return msg;
+}
+
+// percorrendo a lista
+private No getNo(ListaBiblioteca lista, int posicao) {
+    if (posicao < 0 || posicao >= lista.tamanho) {
+        return null;
+    }
+    
+    No atual = lista.cabeca;
+    for (int i = 0; i < posicao; i++) {
+        atual = atual.proximo;
+    }
+    return atual;
+}
 }
